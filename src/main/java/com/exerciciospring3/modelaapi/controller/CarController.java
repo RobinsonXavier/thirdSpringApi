7b181fd711754adede54ac3ab7b554aaa64efd4c
RobinsonXavier <robinson.webzen@gmail.com>
2023-04-03 16:58:35 -0300

    controller and DTO Car created

---
+++ src/main/java/com/exerciciospring3/modelaapi/controller/CarController.java
@@ -0,0 +1,5 @@
+package com.exerciciospring3.modelaapi.controller;
+
+public class CarController {
+  
+}
---
+++ src/main/java/com/exerciciospring3/modelaapi/dto/CarDTO.java
@@ -0,0 +1,5 @@
+package com.exerciciospring3.modelaapi.dto;
+
+public record CarDTO(String modelo, String fabricante, String datadeFabricacao, Double valor, int anoModelo) {
+  
+}
